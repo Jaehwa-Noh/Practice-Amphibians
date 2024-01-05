53c47b4773b2b6d27a1eaee9550ca77a65f5575c
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-05 16:03:22 +0900

    Add fill max size modifier.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansApp.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansApp.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun AmphibiansApp(modifier: Modifier = Modifier) {
-    AmphibiansListScreen(modifier)
+    AmphibiansListScreen(
+        modifier = modifier
+            .fillMaxSize()
+    )
 }
