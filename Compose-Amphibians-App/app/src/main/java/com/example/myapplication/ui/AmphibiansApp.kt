65e7d28ba99f76ee6d5a122533907ca095d168f6
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-05 15:53:10 +0900

    Create AmphibiansApp composable.

---
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansApp.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AmphibiansApp(modifier: Modifier = Modifier) {
+    AmphibiansListScreen(modifier)
+}
