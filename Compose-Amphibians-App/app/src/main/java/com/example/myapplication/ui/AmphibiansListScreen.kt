667c668eac6b56bbea0339421a178f7e20bf9450
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-05 15:53:26 +0900

    Create AmphibiansListScreen composable.

---
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AmphibiansListScreen(modifier: Modifier = Modifier) {
+
+}
