cfb33f14c7c24093361be3f754280d7b5fd01a44
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-07 13:19:14 +0900

    Add basic structure.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
@@ -1,9 +1,36 @@
 package com.example.myapplication.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.myapplication.R
+import com.example.myapplication.model.AmphibiansInfoAPIModel
 
 @Composable
 fun AmphibiansListScreen(modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(emptyList<AmphibiansInfoAPIModel>()) {
+            AmphibiansListItem(amphibian = it)
+        }
+    }
+}
 
+@Composable
+fun AmphibiansListItem(
+    modifier: Modifier = Modifier,
+    amphibian: AmphibiansInfoAPIModel
+) {
+    Card(modifier = modifier) {
+        Text(text = "${amphibian.name}(${amphibian.type}")
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null
+        )
+        Text(text = amphibian.description)
+    }
 }
