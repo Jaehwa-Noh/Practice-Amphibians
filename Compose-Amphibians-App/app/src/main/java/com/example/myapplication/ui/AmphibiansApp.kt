48a211efef8aec294da5b908f2c48311e5050c7d
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-07 14:33:43 +0900

    Add Screen with state.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansApp.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansApp.kt
@@ -1,13 +1,29 @@
 package com.example.myapplication.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun AmphibiansApp(modifier: Modifier = Modifier) {
-    AmphibiansListScreen(
-        modifier = modifier
-            .fillMaxSize()
-    )
+    val amphibiansViewModel: AmphibiansViewModel =
+        viewModel(factory = AmphibiansViewModel.Factory)
+    val amphibiansUiState = amphibiansViewModel.amphibiansUiState.collectAsState()
+
+    when (amphibiansUiState.value) {
+        is AmphibiansUiState.Error -> ErrorScreen(
+            modifier = modifier,
+            errorMessage = (amphibiansUiState.value as AmphibiansUiState.Error).errorMessage,
+            onRetryClick = {
+                amphibiansViewModel.getAmphibiansList()
+            })
+
+        is AmphibiansUiState.Loading -> LoadingScreen(modifier = modifier)
+        is AmphibiansUiState.Success -> AmphibiansListScreen(
+            modifier = modifier,
+            amphibiansList = (amphibiansUiState.value as AmphibiansUiState.Success).amphibians
+        )
+    }
+
 }
