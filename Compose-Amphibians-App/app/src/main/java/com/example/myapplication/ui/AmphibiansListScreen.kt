a192359fa357ad6832596286d40eb511c541fa37
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-07 14:38:54 +0900

    Edit appearance.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.R
 import com.example.myapplication.model.AmphibiansInfoApiModel
 
@@ -19,7 +23,10 @@ fun AmphibiansListScreen(
 
     LazyColumn(modifier = modifier) {
         items(amphibiansList) {
-            AmphibiansListItem(amphibian = it)
+            AmphibiansListItem(
+                amphibian = it,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
@@ -30,11 +37,16 @@ fun AmphibiansListItem(
     amphibian: AmphibiansInfoApiModel
 ) {
     Card(modifier = modifier) {
-        Text(text = "${amphibian.name}(${amphibian.type}")
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
-        )
-        Text(text = amphibian.description)
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "${amphibian.name}(${amphibian.type})",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null
+            )
+            Text(text = amphibian.description)
+        }
     }
 }
