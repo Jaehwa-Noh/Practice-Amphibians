8243c6088157f629d9747a3be8ff81c993fff693
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-07 11:09:35 +0900

    Create AmphibiansRemoteDataSource.

---
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/data/AmphibiansRemoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.data
+
+import com.example.myapplication.model.AmphibiansInfoAPIModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+class AmphibiansRemoteDataSource(
+    private val amphibiansAPI: AmphibiansApi,
+    private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend fun getAmphibiansInfo(): List<AmphibiansInfoAPIModel> {
+        withContext(ioDispatcher) {
+            amphibiansAPI.getAmphibiansInfo()
+        }
+    }
+}
+
+interface AmphibiansApi {
+    suspend fun getAmphibiansInfo(): List<AmphibiansInfoAPIModel>
+}
