7a4134ce9c1ec8ad8d98ea08a0a3e030538d53e6
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-07 20:10:43 +0900

    Add SubcomposeAsyncImage for amphibiansImage.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/ui/AmphibiansListScreen.kt
@@ -1,18 +1,23 @@
 package com.example.myapplication.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.R
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 import com.example.myapplication.model.AmphibiansInfoApiModel
 
 @Composable
@@ -20,7 +25,6 @@ fun AmphibiansListScreen(
     modifier: Modifier = Modifier,
     amphibiansList: List<AmphibiansInfoApiModel>
 ) {
-
     LazyColumn(modifier = modifier) {
         items(amphibiansList) {
             AmphibiansListItem(
@@ -42,9 +46,21 @@ fun AmphibiansListItem(
                 text = "${amphibian.name}(${amphibian.type})",
                 style = MaterialTheme.typography.headlineSmall
             )
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(amphibian.imgSrc)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                loading = {
+                    CircularProgressIndicator(
+                        modifier = Modifier.wrapContentSize()
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
             )
             Text(text = amphibian.description)
         }
