824bc1ab14d9ed54ff240999d22eb2837a9fa51c
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-07 11:09:19 +0900

    Create AmphibiansInfoRepository.

---
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/data/AmphibiansInfoRepository.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.data
+
+import com.example.myapplication.model.AmphibiansInfoAPIModel
+
+interface AmphibiansInfoRepository {
+    suspend fun getAmphibiansInfo(): List<AmphibiansInfoAPIModel>
+}
+
+class NetworkAmphibiansInfoRepository(
+    private val amphibiansApi: AmphibiansApi
+) : AmphibiansInfoRepository {
+    override suspend fun getAmphibiansInfo(): List<AmphibiansInfoAPIModel> {
+        amphibiansApi.getAmphibiansInfo()
+    }
+}
