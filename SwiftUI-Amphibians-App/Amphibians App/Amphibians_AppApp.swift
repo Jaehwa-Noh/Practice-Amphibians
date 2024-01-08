c5b0084e65105eed5f953507fd1c6393de5bacd2
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-08 22:05:01 +0900

    Init project.

---
+++ SwiftUI-Amphibians-App/Amphibians App/Amphibians_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amphibians_AppApp.swift
+//  Amphibians App
+//
+//  Created by Jaehwa Noh on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Amphibians_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
