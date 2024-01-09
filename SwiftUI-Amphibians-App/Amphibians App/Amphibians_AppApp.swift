2d94279babc4ddd7c144c4af0bebc0c63db3bdd0
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-09 15:11:29 +0900

    Add App delegate.

--- SwiftUI-Amphibians-App/Amphibians App/Amphibians_AppApp.swift
+++ SwiftUI-Amphibians-App/Amphibians App/Amphibians_AppApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct Amphibians_AppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
-            AmphibiansApp()
+            AmphibiansApp(appDelegate: appDelegate)
         }
     }
 }
