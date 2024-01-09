a8021ff8331049c4ea583e43fc029da5b3d891e3
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-09 17:04:17 +0900

    Apply List Item.

--- SwiftUI-Amphibians-App/Amphibians App/UI/AmphibiansListInfoView.swift
+++ SwiftUI-Amphibians-App/Amphibians App/UI/AmphibiansListInfoView.swift
@@ -13,7 +13,9 @@ struct AmphibiansLisInfotView: View {
     var body: some View {
         ScrollView {
             LazyVStack {
-                AmphibiansListItem(amphibian: AmphibiansInfoApiModel(name: "", type: "", description: "", imgSrc: ""))
+                ForEach(amphibiansInfo, id: \.name) { amphibianInfo in
+                    AmphibiansListItem(amphibian: amphibianInfo)
+                }
             }
         }
     }
@@ -23,11 +25,44 @@ struct AmphibiansListItem: View {
     let amphibian: AmphibiansInfoApiModel
     
     var body: some View {
-        VStack {
-            Text("")
-            AsyncImage(url: URL(string: ""))
-            Text("")
+        ZStack {
+            VStack(alignment: .leading) {
+                Text("\(amphibian.name)(\(amphibian.type))")
+                    .font(.title2)
+                    
+                AsyncImage(url: URL(string: "\(amphibian.imgSrc)")) {
+                    phase in
+                    if let image = phase.image {
+                        image.resizable()
+                            .scaledToFill()
+                            .frame(height: 200)
+                            .clipped()
+                    } else if phase.error != nil {
+                        HStack {
+                            Spacer()
+                            Image(systemName: "photo")
+                                .frame(height: 200)
+                            Spacer()
+                        }
+                    } else {
+                        HStack {
+                            Spacer()
+                            ProgressView()
+                                .frame(height: 200)
+                            Spacer()
+                        }
+                    }
+                }
+                Text("\(amphibian.description)")
+            }
+            .padding(16)
         }
+        .background(
+            Color.green.opacity(0.1)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 8.0))
+        .shadow(radius: 2)
+        .padding(8)
     }
 }
 
