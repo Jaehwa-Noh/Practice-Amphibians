6474a4f3ba3abd8fbd0eff5f2f21f8638ca69def
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-12 13:42:19 +0900

    Add HiltAndroidApp Annotation.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/AmphibiansApplication.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/AmphibiansApplication.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import android.app.Application
 import com.example.myapplication.data.AppContainer
 import com.example.myapplication.data.DefaultAppContainer
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class AmphibiansApplication : Application() {
     lateinit var container: AppContainer
 
