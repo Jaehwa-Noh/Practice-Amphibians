e9140825d168fa964025051eb89f73eae0f337d3
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-12 13:42:07 +0900

    Add AndroidEntryPoint Annotation.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.AmphibiansApp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,3 +38,5 @@ fun GreetingPreview() {
         AmphibiansApp()
     }
 }
+
+
