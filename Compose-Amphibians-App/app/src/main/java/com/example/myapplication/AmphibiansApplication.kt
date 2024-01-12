1986db4e589445102962e734aa31911cb6c5f75b
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-12 17:37:22 +0900

    Remove AppContainer.

--- Compose-Amphibians-App/app/src/main/java/com/example/myapplication/AmphibiansApplication.kt
+++ Compose-Amphibians-App/app/src/main/java/com/example/myapplication/AmphibiansApplication.kt
@@ -1,16 +1,7 @@
 package com.example.myapplication
 
 import android.app.Application
-import com.example.myapplication.data.AppContainer
-import com.example.myapplication.data.DefaultAppContainer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AmphibiansApplication : Application() {
-    lateinit var container: AppContainer
-
-    override fun onCreate() {
-        super.onCreate()
-        container = DefaultAppContainer()
-    }
-}
+class AmphibiansApplication : Application()
