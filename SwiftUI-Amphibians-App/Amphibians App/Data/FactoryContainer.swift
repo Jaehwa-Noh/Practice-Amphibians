8107477850d391ba3a3f2bd568706d37015693bc
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-20 20:28:48 +0900

    Create amphibiansApiService container.

--- SwiftUI-Amphibians-App/Amphibians App/Data/FactoryContainer.swift
+++ SwiftUI-Amphibians-App/Amphibians App/Data/FactoryContainer.swift
@@ -8,21 +8,11 @@
 import Factory
 
 extension Container {
-    var amphibiansInfoRepository: Factory<AmphibiansInfoRepository> {
+    var amphibiansApiService: Factory<AmphibiansApi> {
         self {
             let baseURL = "https://android-kotlin-fun-mars-server.appspot.com/"
             
-            lazy var amphibiansApi = AmphibiansApiService(baseURL: baseURL)
-            
-            lazy var amphibiansRemoteDataSource = NetworkAmphibiansDataSource(
-                amphibiansApi: amphibiansApi,
-                ioTask: .userInitiated
-            )
-            
-            var amphibiansInfoRepository: AmphibiansInfoRepository {
-                NetworkAmphibiansInfoRepository(amphibiansRemoteDataSource: amphibiansRemoteDataSource)
-            }
-            return amphibiansInfoRepository
+            return AmphibiansApiService(baseURL: baseURL)
         }
         .singleton
     }
