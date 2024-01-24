df6273c25cccb2b83f10da51e2ec81f104bfe972
Jaehwa Noh <shwoghk14@gmail.com>
2024-01-24 15:25:51 +0900

    Update gradle. - Order change.

--- Compose-Amphibians-App/app/build.gradle.kts
+++ Compose-Amphibians-App/app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation("io.ktor:ktor-client-core:2.3.7")
     implementation("io.ktor:ktor-client-okhttp:2.3.7")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+
     androidTestImplementation("androidx.test:runner:1.5.2")
 
     // For instrumentation tests
@@ -87,13 +89,6 @@ dependencies {
     val lifecycleVersion = "2.7.0"
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
-
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
 
     implementation("androidx.core:core-ktx:1.12.0")
